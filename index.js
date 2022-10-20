6705fc010daa23b0323b10abac11c7e75ec1a2c0
SalamanderCtesiphon <swbrookshire@gmail.com>
2022-10-20 16:01:21 -0500

    trying to populate the grid container

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
-const dislayArea = document.querySelector('display-area');
+const container = document.querySelector('container');
+
+for (i = 0, i < 16, i++) {
+    const content = document.createElement('div');
+    content.classList.add('content');
+    container.appendChild(content);
+
+}
