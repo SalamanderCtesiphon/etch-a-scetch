3a561ee0ab107ec1a4998517b5fb6814980290b2
SalamanderCtesiphon <swbrookshire@gmail.com>
2022-10-23 11:19:49 -0500

    added more squares to the container

--- app.js
+++ app.js
@@ -1,9 +1,9 @@
 // a method to populate the container
 let container = document.querySelector('.container');
-container.style.gridTemplateColumns = 'repeat(16 , 1fr)';
-container.style.gridTemplateRows = 'repeat(16 . 1fr)';
+container.style.gridTemplateColumns = 'repeat(100 , 1fr)';
+container.style.gridTemplateRows = 'repeat(100 , 1fr)';
 
-for (let i = 0; i < 256; i++) {
+for (let i = 0; i < 10000; i++) {
     let square = document.createElement('div');
     square.style.backgroundColor = 'whitesmoke';
     container.insertAdjacentElement('beforeend', square);
@@ -13,8 +13,9 @@ for (let i = 0; i < 256; i++) {
 
 const resetButton = document.querySelector('.reset-button');
 
+
 function resetResolution () {
-    alert('placeholder for a prompt');
+    prompt('Please input a number between 2 and 100.')
 }
 
 resetButton.addEventListener('click', resetResolution);
