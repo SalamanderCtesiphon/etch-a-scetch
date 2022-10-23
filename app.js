7897ebbb35e351c1735b62382e0c25c989296df3
SalamanderCtesiphon <swbrookshire@gmail.com>
2022-10-23 17:55:44 -0500

    still trying to color the background with the mouse enter event listener. Currently unsuccessful

--- app.js
+++ app.js
@@ -1,9 +1,9 @@
 // a method to populate the container
 let container = document.querySelector('.container');
-container.style.gridTemplateColumns = 'repeat(100 , 1fr)';
-container.style.gridTemplateRows = 'repeat(100 , 1fr)';
+container.style.gridTemplateColumns = 'repeat(16 , 1fr)';
+container.style.gridTemplateRows = 'repeat(16 , 1fr)';
 
-for (let i = 0; i < 10000; i++) {
+for (let i = 0; i < 256; i++) {
     let square = document.createElement('div');
     square.style.backgroundColor = 'whitesmoke';
     square.classList.add('hover-effect');
@@ -11,7 +11,7 @@ for (let i = 0; i < 10000; i++) {
     container.insertAdjacentElement('beforeend', square);
 }
 
-function changeColor() {
+function changeColor(square) {
     square.style.backgroundColor = 'gray';
 }
 
