e9a67a6d0950309b803223e3db51188e13349a28
SalamanderCtesiphon <swbrookshire@gmail.com>
2022-10-23 20:00:38 -0500

    add a function to take user input and apply it to the resolution of the board

--- app.js
+++ app.js
@@ -1,16 +1,26 @@
 // a method to populate the container
-let container = document.querySelector('.container');
-container.style.gridTemplateColumns = 'repeat(16 , 1fr)';
-container.style.gridTemplateRows = 'repeat(16 , 1fr)';
+function populateBoard (size) {
+    let container = document.querySelector('.container');
+    let squares = container.querySelectorAll('div');
+    squares.forEach((div) => div.remove());
+    container.style.gridTemplateColumns = `repeat(${size} , 1fr)`;
+    container.style.gridTemplateRows = `repeat(${size} , 1fr)`;
 
-for (let i = 0; i < 256; i++) {
-    let square = document.createElement('div');
-    square.style.backgroundColor = 'whitesmoke';
-    square.classList.add('hover-effect');
-    container.insertAdjacentElement('beforeend', square);
+    let amount = size * size;
+    for (let i = 0; i < amount; i++) {
+        let square = document.createElement('div');
+        square.style.backgroundColor = 'whitesmoke';
+        square.classList.add('hover-effect');
+        container.insertAdjacentElement('beforeend', square);
+    }
+
 }
 
+populateBoard(16);
 
+function changeSize (input) {
+    populateBoard(input);
+}
 
 
  
